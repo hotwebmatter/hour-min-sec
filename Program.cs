91e7e558ebafcdd3573c6679aad9701d11a7fee6
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-18 15:55:13 -0500

    Initial commit of hour-min-sec project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hour_min_sec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
