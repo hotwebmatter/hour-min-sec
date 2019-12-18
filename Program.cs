d6e6b72fc7e83475c58aeee4449dacad890323bc
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-18 16:00:08 -0500

    Declare class-level static variables

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@ namespace hour_min_sec
 {
     class Program
     {
+        // use class-level static variables, since
+        // we are not instantiating objects (yet!)
+        static int totalSeconds,
+                   hours,
+                   minutes,
+                   seconds;
         static void Main(string[] args)
         {
             WriteLine("Hello World!");
